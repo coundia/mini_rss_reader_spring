969a58e35f706868658abfafc1931c2822738286
coundia <papacoundia@gmail.com>
2022-04-20 15:37:08 +0000

    fix bug  cors

---
+++ src/main/java/com/coundia/rss/mini_rss_reader_spring/config/ConfigWebFilter.java
@@ -0,0 +1,62 @@
+package com.coundia.rss.mini_rss_reader_spring.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+/**
+ * @author Papa Coundia
+ * @created 20/04/2022 / 15:32
+ * @project mini_rss_reader_spring
+ */
+
+
+
+
+@Configuration
+@EnableWebMvc
+public class ConfigWebFilter implements Filter, WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedMethods("GET, POST, PUT, DELETE");
+    }
+    @Override
+    public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) {
+        HttpServletResponse response = (HttpServletResponse) res;
+        HttpServletRequest request = (HttpServletRequest) req;
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        response.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS, DELETE");
+        response.setHeader("Access-Control-Allow-Headers", "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With, observe, Link, X-Total-Count");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Expose-Headers", "Authorization");
+        response.addHeader("Access-Control-Expose-Headers", "responseType");
+        response.addHeader("Access-Control-Expose-Headers", "observe");
+        response.addHeader("Access-Control-Expose-Headers", "Link");
+        response.addHeader("Access-Control-Expose-Headers", "X-Total-Count");
+        if (!(request.getMethod().equalsIgnoreCase("OPTIONS"))) {
+            try {
+                chain.doFilter(req, res);
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        } else {
+            response.setHeader("Access-Control-Allow-Origin", "*");
+            response.setHeader("Access-Control-Allow-Methods", "POST,GET,DELETE,PUT");
+            response.setHeader("Access-Control-Max-Age", "3600");
+            response.setHeader("Access-Control-Allow-Headers", "Access-Control-Expose-Headers"+"Authorization, content-type," +
+                    "USERID"+"ROLE"+
+                    "access-control-request-headers,access-control-request-method,accept,origin,authorization,x-requested-with,responseType,observe");
+            response.setStatus(HttpServletResponse.SC_OK);
+        }
+
+    }
+}
